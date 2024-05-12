21d0453d47e03e07fee1489c801f235f1331e828
akshat srivastav <akshat1496@gmail.com>
2024-05-12 19:27:25 +0530

    Arrays

---
+++ Arrays/01.js
@@ -0,0 +1,5 @@
+// JavaScript arrays are resizable and can contain a mix of different data types.
+//JavaScript array-copy operations create shallow(Referenced) copies.
+
+
+const MyArray = [0 , 1 , 2 ,True]
